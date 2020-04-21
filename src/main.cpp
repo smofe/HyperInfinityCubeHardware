e6703fc5e5952cd1e4d5bf0cb3fe34a76cd4a397
Michel <michel.klappert@student.hpi.uni-potsdam.de>
2020-04-21 17:56:22 +0200

    first led implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,46 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include "FireBaseESP32.h"
+#include <FastLED.h>
 
 #define FIREBASE_HOST "https://hyperinfinitycube.firebaseio.com/"
 #define FIREBASE_AUTH "AZ7ia8jJRJXjh9U9BjE0V1tFwn7Oclq5KUSaf52a"
 
-const char* ssid = "FRITZ!Box WLAN 3370";
-const char* password = "0045939574773306";
+#define NUM_LEDS 300
+
+const char* ssid = "MirDochEgal";
+const char* password = "93153805";
+
+CRGBArray<NUM_LEDS> leds;
+const int ledPin = 25;
 
 FirebaseData firebaseData;
+boolean power = false;
+int solidColor = 0;
+
+CRGB getRGBColorAtRoot(String root){
+  int r,g,b;
+
+  if (Firebase.getInt(firebaseData,root + "/r")){
+      r = firebaseData.intData();
+    } else {
+      Serial.println(firebaseData.errorReason());
+  }
+  
+  if (Firebase.getInt(firebaseData,root + "/g")){
+      g = firebaseData.intData();
+    } else {
+      Serial.println(firebaseData.errorReason());
+  }
+  
+  if (Firebase.getInt(firebaseData,root + "/b")){
+      b = firebaseData.intData();
+    } else {
+      Serial.println(firebaseData.errorReason());
+    }
+  
+  return CRGB(r,g,b);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -29,15 +61,29 @@ void setup() {
   Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
   
+  pinMode(ledPin,OUTPUT);
+  FastLED.addLeds<WS2812B, ledPin, GRB>(leds, NUM_LEDS);
+
   }
 
 void loop() {
-  if (Firebase.getString(firebaseData, "/power")) {
+  if (Firebase.getBool(firebaseData, "/power")) {
 
-    Serial.println(firebaseData.stringData());
+    power = firebaseData.boolData();
 
   } else {
     Serial.println(firebaseData.errorReason());
   } 
-  delay(1000);
+  
+
+  if (power){
+    leds.fill_solid(getRGBColorAtRoot("color"));
+    FastLED.show();
+  } else {
+    leds.fill_solid(CRGB::Black);
+    FastLED.show();
+    Serial.println("power off");
+  }
+  delay(50);
 }
+
